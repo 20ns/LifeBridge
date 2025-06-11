1134846955437e15de09bbcde4a24f5d58501a73
Navpreet Singh <ns.2004@outlook.com>
2025-06-11 14:32:34 +0100

    Fix Jest configuration and resolve major test issues

---
+++ tests/jest.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', {
+      presets: [
+        ['@babel/preset-env', { targets: { node: 'current' } }],
+        '@babel/preset-react',
+        '@babel/preset-typescript'
+      ]
+    }]
+  },
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
+  testMatch: [
+    '**/__tests__/**/*.(js|jsx|ts|tsx)',
+    '**/*.(test|spec).(js|jsx|ts|tsx)'
+  ],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/../frontend/src/$1'
+  },
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/.next/**'
+  ],
+  transformIgnorePatterns: [
+    '/node_modules/(?!(lucide-react)/)'
+  ],
+  testTimeout: 10000
+};
